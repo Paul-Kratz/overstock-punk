a564c3019eaa33c38a29c47273a1f43ce0bdbb66
Paul-Kratz <kratzpaul95@gmail.com>
2022-03-11 10:59:51 +0000

    Add infinite loading and homepage property to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React, { useEffect } from 'react';
-import { getAllBeers, getBeerById } from './api';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { getAllBeers } from './api';
 import './App.css';
+import { IBeer } from './models/IBeer';
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8
+};
 
 function App() {
+  const [items, setItems] = useState<IBeer[]>([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const getData = async () => {
-    const beers = await getAllBeers();
+    const beers = await getAllBeers({ page });
 
-    const beer = await getBeerById(1)
-    console.log(beers)
-
-    console.log(beer)
+    if (beers.length === 0) {
+      setHasMore(false);
+    }
+    setItems(items.concat(beers));
+    setPage(page + 1)
   }
   useEffect(() => {
     getData();
   }, [])
   return (
-    <div>
-      Hello World
-    </div>
+    <InfiniteScroll
+      dataLength={items.length}
+      next={getData}
+      hasMore={hasMore}
+      style={{ overflow: 'none' }}
+      loader={<h4>Brewing another batch...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Never drink & drive</b>
+        </p>
+      }
+    >
+
+      {items.map((i, index) => (
+        <div style={style} key={index}>
+          <img src={i.image_url} alt={`Image of ${i.name}`} height={50} />
+          {i.name}
+        </div>
+      ))}
+    </InfiniteScroll>
   );
 }
 
